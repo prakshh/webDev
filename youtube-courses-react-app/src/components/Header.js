f286041ce15d2bf2e9aecd1b4652c55d8bbe561f
“prakshh” <prakashdas18@gmail.com>
2021-10-27 19:55:36 +0530

    jumbotron reactstrap

---
+++ youtube-courses-react-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default Header;
+
+function Header({name, title}){
+    return (
+        <div>
+            <h1>Header component</h1>
+        </div>
+    );
+
+}
