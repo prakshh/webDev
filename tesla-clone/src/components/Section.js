4450b038d5c567bcf1e97ddd857a8baa53c54440
“prakshh” <prakashdas18@gmail.com>
2021-11-20 11:14:12 +0530

    ButtonGroup styling

--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import styled from "styled-components"
 
+
 function Section() {
     return (
         <Wrap>
-            Section
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order online for touchless delivery</p>
+            </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
         </Wrap>
     )
 }
@@ -14,5 +26,38 @@ export default Section
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background: orange;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; //vertical alignment
+    align-items: center; // horizontal alignment
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;  
+    cursor: pointer;  
+
+`
+const RightButton = styled(LeftButton)`
 `
