694eba1cb09e095c7d2d706ff4f8e2dd3f76d0c0
“prakshh” <prakashdas18@gmail.com>
2021-11-26 15:33:50 +0530

    using Redux to create carSlice

--- tesla-clone/src/app/store.js
+++ tesla-clone/src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+// commented because counter folder deleted from features folder
+import carReducer from "../features/car/carSlice"
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    car: carReducer
   },
 });
--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 import styled from "styled-components"
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import { selectCars } from '../features/car/carSlice'
+import { useSelector } from 'react-redux';
 
 function Header() {
     const[burgerStatus, setBurgerStatus] = useState(false);
-
+    const cars = useSelector(selectCars)
+    console.log(cars);
+    
     return (
         <Container>
             <a>
