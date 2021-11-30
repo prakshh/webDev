90ba0032c25c3763e09c40efd573433ca605171b
“prakshh” <prakashdas18@gmail.com>
2021-11-30 11:29:32 +0530

    Middlewares and First Request

--- social-media-app/node-rest-api/index.js
+++ social-media-app/node-rest-api/index.js
@@ -15,6 +15,17 @@ mongoose.connect(
     }
   );
   
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req,res)=>{
+    res.send("welcome to homepage")
+})
+app.get("/users", (req,res)=>{
+    res.send("welcome to users page")
+})
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
