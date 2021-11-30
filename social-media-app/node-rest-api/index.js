af778d91e6525147d78339233785121dc0ad7cc5
“prakshh” <prakashdas18@gmail.com>
2021-11-30 13:15:57 +0530

    user model created

--- social-media-app/node-rest-api/index.js
+++ social-media-app/node-rest-api/index.js
@@ -10,13 +10,21 @@ const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(
-    process.env.MONGO_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log("Connected to MongoDB");
-    }
-  );
+// mongoose.connect(
+//     process.env.MONGO_URL,
+//     { useNewUrlParser: true, useUnifiedTopology: true },
+//     () => {
+//       console.log("Connected to MongoDB");
+//     }
+//   );
+
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true})
+  .then(() => console.log("Database connected!"))
+  .catch(err => console.log(err));
   
 //middleware
 app.use(express.json());
@@ -26,12 +34,12 @@ app.use(morgan("common"));
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 
-app.get("/", (req,res)=>{
-    res.send("welcome to homepage")
-})
-app.get("/users", (req,res)=>{
-    res.send("welcome to users page")
-})
+// app.get("/", (req,res)=>{
+//     res.send("welcome to homepage")
+// })
+// app.get("/users", (req,res)=>{
+//     res.send("welcome to users page")
+// })
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
--- social-media-app/node-rest-api/routes/auth.js
+++ social-media-app/node-rest-api/routes/auth.js
@@ -1,8 +1,19 @@
 const router = require("express").Router()
+const User = require("../models/User");
 
-router.get("/", (req,res) => {
-    res.send("hey, it's auth route")
-})
+// RESGISTER
+router.get("/register", async(req,res) => {
+    const user = await new User({
+        username: "john",
+        email: "john@gmail.com",
+        password: "123456"
+    })
+    await user.save();
+    res.send("ok");
+});
+// router.get("/", (req,res) => {
+//     res.send("hey, it's auth route")
+// })
 
 //to use it to index.js, export it
 module.exports = router
